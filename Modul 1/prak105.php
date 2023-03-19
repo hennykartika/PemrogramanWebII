3bb067cd4e6cca2bdaa1a8acb19d6f9864d2925d
hennykartika <93182127+hennykartika@users.noreply.github.com>
2023-03-19 14:43:18 +0800

    Update prak105.php

--- Modul 1/prak105.php
+++ Modul 1/prak105.php
@@ -1,57 +1,45 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Daftar Smartphone Samsung</title>
+	<title>PRAKTIKUM 1</title>
 	<style>
 		table {
-			border-collapse: collapse;
-			width: 100%;
-			border: 5px double black;
+			border-collapse: separate;
+			border: 2px solid black;
 			background-color: white;
 		}
 		th, td {
-			text-align: left;
-			padding: 8px;
-			border-bottom: 2px double white;
-			border-right: 2px double white;
-			border: double;
-			color: black;
+			border: 1px solid black;
 		}
 		th {
-			background-color: #ff0000;
-			border-right: 2px double white;
-			border: double;
-			padding: 16px;
-			font-size: 24px;
-		}
-		tr:first-child th {
-			border-top: 2px double black;
-		}
-		tr:last-child td {
-			border-bottom: none;
+			background-color: red;
+			font-weight: bold;
+			font-size: 25px;
 		}
 	</style>
 </head>
 <body>
-	<?php
-	// Deklarasi associative array daftar smartphone Samsung
-	$samsung_smartphones = array(
-		"Samsung Galaxy S22",
-		"Samsung Galaxy S22+",
-		"Samsung Galaxy A03",
-		"Samsung Galaxy Xcover 5"
-	);
-	?>
+	<h1></h1>
 	<table>
-		<tr>
-			<th>Daftar Smartphone Samsung</th>
-		</tr>
-		<?php
-		// Looping associative array untuk menampilkan setiap elemen sebagai baris tabel
-		foreach($samsung_smartphones as $smartphone) {
-			echo "<tr><td>" . $smartphone . "</td></tr>";
-		}
-		?>
+		<thead>
+			<tr>
+				<th>Daftar Smartphone Samsung</th>
+			</tr>
+		</thead>
+		<tbody>
+			<tr>
+				<td>Samsung Galaxy S22</td>
+			</tr>
+			<tr>
+				<td>Samsung Galaxy S22</td>
+			</tr>
+			<tr>
+				<td>Samsung Galaxy A03</td>
+			</tr>
+			<tr>
+				<td>Samsung Galaxy Xcover 5</td>
+			</tr>
+		</tbody>
 	</table>
 </body>
 </html>
